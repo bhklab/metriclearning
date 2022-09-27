53c8b234ecf2a9ad165d58e26d2efe9aea6720f9
Ian Smith <iamsinht@gmail.com>
2022-09-27 06:42:11 -0400

    Additional experiment functions.

--- experimentFunctions.R
+++ experimentFunctions.R
@@ -1,5 +1,32 @@
-# Analyze L1000 datasets
+library(CMAPToolkit)
+library(cmapR)
 
+# Analyze L1000 datasets
+analyzeL1KData <- function(dspath, metapath, cell_id, outpath=".", method="xval", epochs=10){
+  l1k_meta <- read_l1k_meta(metapath, version=2020)
+  
+  if (cell_id == "all"){
+    mysigs <- l1k_meta$siginfo[l1k_meta$siginfo$pert_type == "trt_cp",]
+  } else {
+    mysigs <- l1k_meta$siginfo[l1k_meta$siginfo$cell_id == cell_id & l1k_meta$siginfo$pert_type == "trt_cp",]
+  }
+  
+  ds <- parse_gctx(fname=get_level5_ds(dspath), rid=l1k_meta$landmarks$pr_gene_id, cid=mysigs$sig_id)
+  
+  if (method == "xval"){
+    nFolds <- 5
+    
+    L1Kxval <- metricCrossValidate(t(ds@mat), mysigs$pert_iname, nFolds=nFolds, epochs=epochs)
+    saveRDS(L1Kxval, file=file.path(outpath, sprintf("L1Kxval_epch=%d_folds=%d_cell=%s.rds", epochs, nFolds, cell_id)))
+    return(L1Kxval)
+  } else if (method == "allds"){
+    L1Kmetric <- learnInnerProduct(t(ds@mat), mysigs$pert_iname, epochs=epochs)
+    saveRDS(L1Kmetric, file=file.path(outpath, sprintf("L1Kmetric_epch=%d_cell=%s.rds", epochs, cell_id)))
+    return(L1kmetric)
+  } else if (method == "ntraining"){
+    
+  }
+}
 
 
 
